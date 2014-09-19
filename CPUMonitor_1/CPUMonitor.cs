287d21bc5d345d79f33d04119e8a07ddeb2a25b0
Cotes <cristian.cotes@urv.cat>
2014-09-19 15:24:34 +0200

    Refactor

--- CPUMonitor_1/CPUMonitor.cs
+++ CPUMonitor_1/CPUMonitor.cs
@@ -8,16 +8,18 @@ using System.Threading;
 namespace CPUMonitor_1
 {
 
-    public class Monitor {
-
+    class CPUMonitor
+    {
         private static bool finish = false;
         private static LinkedList<float> cpuValues = new LinkedList<float>();
 
-        public static void ThreadProc() {
-            Monitor.Start();
+        public static void ThreadProc()
+        {
+            CPUMonitor.Start();
         }
 
-        public static void Start() {
+        public static void Start()
+        {
 
             PerformanceCounter cpuCounter;
             cpuCounter = new PerformanceCounter();
@@ -25,7 +27,7 @@ namespace CPUMonitor_1
             cpuCounter.CategoryName = "Processor";
             cpuCounter.CounterName = "% Processor Time";
             cpuCounter.InstanceName = "_Total";
-            
+
             while (!finish)
             {
                 Thread.Sleep(100);
@@ -41,14 +43,10 @@ namespace CPUMonitor_1
             return cpuValues;
         }
 
-    }
-
-    class Program
-    {
         static void Main(string[] args)
         {
-            Monitor monitor = new Monitor();
-            Thread t = new Thread(new ThreadStart(Monitor.ThreadProc));
+            CPUMonitor monitor = new CPUMonitor();
+            Thread t = new Thread(new ThreadStart(CPUMonitor.ThreadProc));
             t.Start();
             Thread.Sleep(12000);
             monitor.end();
